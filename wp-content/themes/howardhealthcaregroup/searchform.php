609c7ea1b426153f63e4976ce774b0bbca8ccf64
Mark Win Panerio <mwpanerio@webfx.com>
2022-09-16 23:20:45 +0800

    Hotfix: Update search for blog sidebar

--- wp-content/themes/howardhealthcaregroup/searchform.php
+++ wp-content/themes/howardhealthcaregroup/searchform.php
@@ -6,5 +6,8 @@
 ?>
 <form action="/" method="get">
     <input type="text" name="s" id="s" value="<?php echo get_search_query( true ); ?>" data-swplive="true" placeholder="Search Our Site">
+    <?php if(is_home() || is_archive() || is_single()): ?>
+        <input type="hidden" name="post_type" value="post">
+    <?php endif; ?>
     <button type="submit"><i class="icon-search"></i></button>
 </form>
--- wp-content/themes/howardhealthcaregroup/sidebar.php
+++ wp-content/themes/howardhealthcaregroup/sidebar.php
@@ -1,3 +1,13 @@
 <aside class="page-sidebar">
+    <div class="widget">
+        <h2>Search Our Blog</h2>
+        <div class="sidebar-search">
+            <form action="/" method="get">
+                <input type="text" name="s" id="s" value="<?php echo get_search_query( true ); ?>" data-swplive="true" placeholder="Search for a Topic">
+                <input type="hidden" name="post_type" value="post">
+                <button type="submit"><i class="icon-search"></i></button>
+            </form>
+        </div>
+    </div>
     <?php dynamic_sidebar( 'sidebar' ); ?>
 </aside>
