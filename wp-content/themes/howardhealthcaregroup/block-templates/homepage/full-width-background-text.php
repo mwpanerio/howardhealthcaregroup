be587935144eac4af3659141d0c3e242dbeaac68
Mark Win Panerio <mwpanerio@webfx.com>
2022-09-22 10:39:03 +0800

    Update added spices in homepage

--- wp-content/themes/howardhealthcaregroup/block-templates/homepage/full-width-background-text.php
+++ wp-content/themes/howardhealthcaregroup/block-templates/homepage/full-width-background-text.php
@@ -1,19 +1,22 @@
-<section class="full-width-image-text section-padding full-width-image-text--box-overlay full-width-image-text--direct-overlay">
+<section class="full-width-image-text section-padding full-width-image-text--box-overlay full-width-image-text--homepage full-width-image-text--direct-overlay js-fullwidth-overlay-homepage">
     <?php echo fx_get_image_tag(get_field('background_image'), 'full-width-image-text__img object-fit'); ?>
     <div class="container">
         <div class="row">
             <div class="col-lg-10 col-lg-offset-1">
-                <div class="full-width-image-text__content">
-                    <h5><span> <?php echo get_field('subheading'); ?></span></h5>
-                    <h2 class="full-width-image-text__headline"><?php echo get_field('title'); ?></h2>
-                    <div class="full-width-image-text__description">
-                        <div class="read-more__box">
-                            <div class="read-more__content js-read-more">
-                                <div class="read-more__wrapper">
-                                    <?php echo get_field('description'); ?>
+                <div class="full-width-image-text__wrapper">
+                    <div class="full-width-image-text__overlay"></div>
+                    <div class="full-width-image-text__content">
+                        <h5><span> <?php echo get_field('subheading'); ?></span></h5>
+                        <h2 class="full-width-image-text__headline"><?php echo get_field('title'); ?></h2>
+                        <div class="full-width-image-text__description">
+                            <div class="read-more__box">
+                                <div class="read-more__content js-read-more">
+                                    <div class="read-more__wrapper">
+                                        <?php echo get_field('description'); ?>
+                                    </div>
                                 </div>
+                                <a class="expand js-read-more-expand" href="javascript:;">Read More</a>
                             </div>
-                            <a class="expand js-read-more-expand" href="javascript:;">Read More</a>
                         </div>
                     </div>
                 </div>
